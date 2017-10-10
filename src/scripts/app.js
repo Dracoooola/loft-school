6f0c20cc007f45ea4aebab95318b0d8866469749
Vlad Klimov <klimv97@mail.ru>
2017-10-11 00:42:08 +0300

    parallax1.0

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,3 +2,27 @@ const slider = require('./common/slider');
 const $ = require('jquery'); // если будет нужен
 
 slider(); // инициализируем слайдер
+
+var parallaxContainer = document.getElementById('parallax'),
+    layers = parallaxContainer.children;
+
+var moveLayers = function(e) {
+    var initialX = (window.innerWidth / 2) - e.pageX;
+    var initialY = (window.innerHeight / 2) - e.pageY;
+
+    [].slice.call(layers).forEach(function(layer, index) {
+        var
+            divider = index / 60,
+            positionX = initialX * divider,
+            positionY = initialY * divider,
+            bottomPosition = (window.innerHeight / 2) * divider,
+            transformString = 'translate(' + positionX + 'px,' + positionY + 'px)',
+            image = layer.firstElementChild;
+
+        layer.style.transform = transformString;
+        image.style.bottom = '-' + bottomPosition + 'px';
+    });
+
+};
+
+window.addEventListener('mousemove', moveLayers);
